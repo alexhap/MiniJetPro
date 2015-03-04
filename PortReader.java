d69eeb39a34c9ac79d3ba44ed457df7db2ba5e38
alexhap <alexhap@users.noreply.github.com>
2015-03-04 14:21:12 +0200

    Create PortReader.java

---
+++ PortReader.java
@@ -0,0 +1,35 @@
+/**
+ * Created by alex on 18.02.2015.
+ *
+ */
+
+import java.util.Observable;
+import jssc.SerialPort;
+import jssc.SerialPortEvent;
+import jssc.SerialPortEventListener;
+import jssc.SerialPortException;
+
+class PortReader extends Observable implements SerialPortEventListener {
+    private final SerialPort serialPort;
+
+    public PortReader(SerialPort sp) {
+        serialPort = sp;
+    }
+
+    public void serialEvent(SerialPortEvent event) {
+        try {
+            Thread.sleep(20);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        if (event.getEventValue() > 0) {
+            try {
+                notifyObservers(serialPort.readBytes());
+                this.setChanged();
+            } catch (SerialPortException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
